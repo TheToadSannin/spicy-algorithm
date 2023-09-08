e8e17f67525a8323ff3794d9a844cf5cea11825a
Gaurav panwar <gp771234@gmail.com>
2023-09-08 23:11:47 +0530

    asdflkjads

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
             }
         }
         
-    } , [isLoading, authenticated])
+    } , [isLoading, authenticated, navigate])
 
     
     const handleReviewSubmit = async() => {
